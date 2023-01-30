54d61671308d575b21784b598415b60b9b022abe
sumaiya357 <rahmansumaiya75@gmail>
2023-01-31 00:56:55 +0600

    best selling product card  ready

--- src/pages/Home/InfoCards/InfoCard.js
+++ src/pages/Home/InfoCards/InfoCard.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-const InfoCard = ({singlebestSellProduct}) => {
+const InfoCard = ({ singlebestSellProduct }) => {
 
-    const {name, img, price} = singlebestSellProduct;
+    const { name, img, price } = singlebestSellProduct;
     return (
-        <div className="card card-side bg-base-100 shadow-xl border-slate-300 m-1">
-           <div className=''> <figure><img className='' src={img}/></figure></div>
+
+        <div className="card w-56 bg-base-100 shadow-xl rounded">
+            <figure className=""><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>{price}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy</button>
+                <h2 className="card-title">
+                {name}
+                   
+                </h2>
+                <p className='font-bold ' >{price} <span className='text-2xl'>৳</span></p>
+                <div className="card-actions justify-start">
+                <button className="btn btn-success btn-sm text-xs button text-white  ">Shop Now</button>
                 </div>
             </div>
         </div>
+
+
+        // <div className="card card-side bg-base-100 shadow-xl border-slate-300 m-1">
+        //    <div className=''> <figure><img className='' src={img}/></figure></div>
+        //     <div className="card-body">
+        //         <h2 className="card-title">{name}</h2>
+        //         <p>{price}</p>
+        //         <div className="card-actions justify-end">
+        //             <button className="btn btn-primary">Buy</button>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
--- src/pages/Home/InfoCards/InfoCards.js
+++ src/pages/Home/InfoCards/InfoCards.js
@@ -1,54 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import InfoCard from './InfoCard';
 
 const InfoCards = () => {
 
-    const bestSell = [
-        {
-            id: 1,
-            name: "Bonsai",
-            img: "https://cdn.shopify.com/s/files/1/2441/5983/products/AmuseableBonsaiTree_700x.jpg?v=1655818112",
-            price: 3000
-        },
-        {
-            id: 2,
-            name: "Lotus",
-            img: "https://i.pinimg.com/736x/f7/3c/72/f73c72d63b2b0821e8ebc72ab5ff343e.jpg",
-            price: 1000
-        },
-        {
-            id: 3,
-            name: "Pink Rose",
-            img: "https://www.thefragrantrosecompany.co.uk/media/catalog/product/cache/92cbed5f29d6c3826fd0716e473f2bfa/m/i/millie-garden-rose-1.jpg",
-            price: 2000
-        },
-        {
-            id: 4,
-            name: "Tomato",
-            img: "https://laidbackgardener.blog/wp-content/uploads/2018/02/20180214a-sweetvalentinehybridtomato-www-hpsseed-com.jpg",
-            price: 700
-        },
-        {
-            id: 5,
-            name: "Aloe Vera",
-            img: "https://pictures.kare-design.com/6/KARE-60724-700x700.jpg",
-            price: 1500
-        },
-        {
-            id: 6,
-            name: "Strawberry",
-            img: "https://www.thegardenshop.ie/images/detailed/6/alpine-strawberry-plants.jpg",
-            price: 4000
-        },
-
-    ]
+    const [bestSell, setBestSell] = useState([]) ;
+    useEffect( () => {
+        fetch('BestSell.json')
+        .then(res => res.json())
+        .then(data=> setBestSell(data))
+    })
     return (
-        <div className='grid grid-cols-2 lg:grid-cols-2'>
+        <div className='grid grid-cols-2 lg:grid-cols-2  p-5'>
              
-            <div>
-            <h4 className='bg-green-700 text-primary-content mt-5 mb-12 p-3'>OUR BEST SELLLING PRODUCTS</h4>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className=''> {/* gap-x-8 gap-y-4 */}
+            <h4 className='bg-green-700 text-primary-content  mt-5 mb-12 p-3'>OUR BEST SELLLING PRODUCTS</h4>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-4 '>
                
                {
                    bestSell.map(singlebestSellProduct => <InfoCard
@@ -60,11 +27,15 @@ const InfoCards = () => {
                }
            </div >
             </div>
-            <div className=' flex justify-center mt-5 mb-12 ml-20 mr-5 bg-gray-300'>
-                <ul className="menu menu-compact bg-base-400 rounded-box mt-3">
+            
+
+            <div className=' flex justify-center  '>
+            <div className=' flex justify-center w-56 mt-5 mb-12 ml-20 mr-5 bg-gray-300 h-[42rem] lg:h-[32rem] p-2'>
+            {/* h-[32rem] */}
+                <ul className="menu menu-compact bg-base-400 rounded-box mt-3 ">
                     <h4 className='text-xl font-bold mb-5'>Category</h4>
                     <>
-                    <Link className='mb-4 font-bold font-serif'>Flower Plant</Link> 
+                    <Link className='mb-4 font-bold font-serif pt-8'>Flower Plant</Link> 
                     <Link className='mb-4 font-bold font-serif'>Indoor Plant</Link>
                     <Link className='mb-4 font-bold font-serif'>Fruit Plant</Link> 
                     <Link className='mb-4 font-bold font-serif'>Bonsai Plant</Link>  
@@ -76,6 +47,7 @@ const InfoCards = () => {
                    
                     </>
                 </ul>
+            </div>
             </div>
         </div>
     );
