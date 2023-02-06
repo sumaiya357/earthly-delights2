1a303b7b4e8378469e40a85d5ca33ca87da549d2
sumaiya357 <rahmansumaiya75@gmail>
2023-02-06 21:08:45 +0600

    category connected to navbar for large and small screen

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { faArrowAltCircleDown, faArrowDown, faArrowDown19, faArrowDownShortWide, faArrowsDownToLine, faArrowsDownToPeople, faArrowsUpDown, faArrowsUpDownLeftRight, faFileDownload, faLongArrowAltDown, faShoppingCart, faTurnDown } from '@fortawesome/free-solid-svg-icons';
+import { faTurnDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -7,30 +7,29 @@ import Category from '../Category/Category';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar  bg-green-700 text-primary-content mt-5">
+            <div className="navbar  bg-green-700 text-primary-content mt-5 ">
 
 
 
                 {/* --------------//--------- FOR SMALL SCREEN //--------------//*/}
 
 
-                <div className="navbar-start ">
-                    <div className="dropdown">
+                <div className="navbar-start  ">
+                    <div className="dropdown ">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-green-700 rounded-box w-52">
                             <li><Link to='/'>Home</Link></li>
 
                             <li tabIndex={0}>
-                                <a className="justify-between">
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
+                            <Link to='/' className="justify-between">
+                               Category
+                                <FontAwesomeIcon icon={faTurnDown}></FontAwesomeIcon>
+                                </Link>
+                            <ul className="p-2">
+                            <li className='bg-amber-400'><Category></Category></li>
+                            </ul>
                             </li>
                             <li><Link to='/shop'>Shop</Link></li>
                             <li><Link to='/order'>Order</Link></li>
